505bb50e410f82d997eb6d849ce2a9019f9f5823
rusya07 <twitchaimgoose@gmail.com>
2021-12-28 19:25:55 +0600

    do Lesson 7

---
+++ oop2.js
@@ -0,0 +1,42 @@
+// Инкапсуляция
+
+class Car {
+
+    _speed = 0;
+
+    constructor(name, model, maxSpeed, color, weight, speed) {
+        this.name = name;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.color = color;
+        this.weight = weight;
+        this.speed = speed;
+    }
+
+    gas(){
+        if (this._speed < this.maxSpeed){
+            this._speed += this.speed;
+            console.log(this._speed)
+        }else{
+            console.log("MAX SPEED! CHECK!")
+        }
+    }
+}
+
+const bmw = new Car("BMW", "E 38", 450, "BLACK", 2.5, 50);
+console.log(bmw)
+bmw.gas()
+bmw.gas()
+bmw.gas()
+bmw.gas()
+bmw.gas()
+bmw.gas()
+bmw.gas()
+
+const lexus = new Car("Lexus", "IS 250", 350, "WHITE", 1.5, 20);
+console.log(lexus)
+lexus.gas()
+lexus.gas()
+lexus.gas()
+lexus.gas()
+lexus.gas()
